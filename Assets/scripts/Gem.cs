daffc9f6648bee89cc182d5cecf9191bc8d5a8cc
Dethrail <max.od.ua@gmail.com>
2019-11-23 21:48:16 +0200

    Revert "move to Match3 namespace"
    This reverts commit 395e3c053e67b1cd57badd9ba2838cc93c644d3f.

---
+++ Assets/scripts/Gem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gem : MonoBehaviour
+{
+    public Image Foreground;
+    public Image Background;
+    public Vector2Int Position;
+    public GemColor Color;
+    public Text Text;
+}
+
+public enum GemColor
+{
+    None = 0,
+    Blue = 1,
+    Yellow,
+    Red,
+    Purple,
+    Green,
+}
---
+++ Assets/scripts/GemFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GemFactory : MonoBehaviour
+{
+    public List<Gem> GemPrefabs;
+    private static GemFactory _instance;
+
+    public void Awake()
+    {
+        _instance = this;
+    }
+
+    public static Gem CreateGem(GemColor color)
+    {
+        Gem gem = Instantiate(_instance.GemPrefabs.First(x => x.Color == color));
+        return gem;
+    }
+}
