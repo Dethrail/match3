265cbad24923f7954dc09d8f493e5da2542ded19
Dethrail <max.od.ua@gmail.com>
2019-11-24 15:17:25 +0200

    split classes

--- Assets/scripts/GameField.cs
+++ Assets/scripts/GameField.cs
@@ -215,18 +215,3 @@ public class GameField : MonoBehaviour
         return false;
     }
 }
-
-public class Sequence
-{
-    public Vector2Int InitialPosition { get; }
-    public Vector2Int Move { get; }
-    public List<Vector2Int> Vertical = new List<Vector2Int>();
-    public List<Vector2Int> Horizontal = new List<Vector2Int>();
-    public int LongestSequence;
-
-    public Sequence(Vector2Int position, Vector2Int move)
-    {
-        InitialPosition = position;
-        Move = move;
-    }
-}
---
+++ Assets/scripts/Sequence.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sequence
+{
+    public Vector2Int InitialPosition { get; }
+    public Vector2Int Move { get; }
+    public List<Vector2Int> Vertical = new List<Vector2Int>();
+    public List<Vector2Int> Horizontal = new List<Vector2Int>();
+    public int LongestSequence;
+
+    public Sequence(Vector2Int position, Vector2Int move)
+    {
+        InitialPosition = position;
+        Move = move;
+    }
+}
